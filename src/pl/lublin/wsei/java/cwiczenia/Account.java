cb476a3652bd049cf531d65ca9d132957462a9c0
Dawid <dawiddrazyk99@o2.pl>
2021-06-30 14:39:11 +0200

    Initial commit

---
+++ src/pl/lublin/wsei/java/cwiczenia/Account.java
@@ -0,0 +1,11 @@
+package pl.lublin.wsei.java;
+private String name;
+public void setName(String name) {
+        this.name = name;
+        }
+public String getName() {
+        return name;
+        }
+
+public class Account {
+}
---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,6 @@
+package pl.lublin.wsei.java;
+Account acc = new Account();
+        acc.setName("Piotr Gołabek");
+        System.out.println(acc.getName());
+public class Main {
+}
